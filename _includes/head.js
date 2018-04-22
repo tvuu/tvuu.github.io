5819ea941141619b584d289c4e3d28e2fb3df872
tvuu <thinh.vuu@gmail.com>
2018-04-22 00:19:39 -0400

    commit1

---
+++ _includes/head.js
@@ -0,0 +1,30 @@
+document.write('\
+    <meta charset="utf-8">\
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\
+    <meta name="description" content="">\
+    <meta name="author" content="">\
+\
+    <!-- Icons -->\
+    <meta name="msapplication-TileColor" content="#ffffff" />\
+    <meta name="msapplication-TileImage" content="/img/icon/cat/cat--5-144-85909.png">\
+    <link rel="icon" type="image/x-icon" href="/img/icon/cat/favicon.ico">\
+    <link rel="apple-touch-icon-precomposed" href="/img/icon/cat/cat--5-152-85909.png">\
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/icon/cat/cat--5-152-85909.png">\
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/icon/cat/cat--5-144-85909.png">\
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/icon/cat/cat--5-120-85909.png">\
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/icon/cat/cat--5-114-85909.png">\
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/icon/cat/cat--5-72-85909.png">\
+    <link rel="apple-touch-icon-precomposed" href="/img/icon/cat/cat--5-57-85909.png">\
+\
+<link rel="icon" href="/img/icon/cat/cat--5-32-85909.png" sizes="32x32">\
+\
+    <!-- Bootstrap core CSS -->\
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">\
+\
+    <!-- Custom fonts for this template -->\
+    <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">\
+    <link href=\'https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic\' rel=\'stylesheet\' type=\'text/css\'>\
+    <link href=\'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800\' rel=\'stylesheet\' type=\'text/css\'>\
+\
+    <!-- Custom styles for this template -->\
+    <link href="/css/clean-blog.min.css" rel="stylesheet">\');
